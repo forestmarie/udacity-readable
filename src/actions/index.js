e1de3293ce52144a1052afa3ca82f4ffaa4a83ec
forestmarie <forest.marie@gmail.com>
2017-09-14 20:59:22 -0500

    feat: added serve side code.

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,126 +13,126 @@ export const FETCH_COMMENTS_HAS_ERRORED = "FETCH_COMMENTS_HAS_ERRORED";
 export const FETCH_COMMENTS_LOADING = "FETCH_COMMENTS_LOADING";
 
 export function addPost({ author, title, body }) {
-    return {
-        type: ADD_POST,
-        author,
-        title,
-        body
-    };
+  return {
+    type: ADD_POST,
+    author,
+    title,
+    body
+  };
 }
 
 export function likePost({ postId }) {
-    return {
-        type: LIKE_POST,
-        postId
-    };
+  return {
+    type: LIKE_POST,
+    postId
+  };
 }
 
 export function editPost({ author, title, body, postId }) {
-    return {
-        type: EDIT_POST,
-        author,
-        title,
-        body, 
-        postId
-    };
+  return {
+    type: EDIT_POST,
+    author,
+    title,
+    body,
+    postId
+  };
 }
 
 export function fetchPostsHasErrored() {
-    return {
-        type: FETCH_POSTS_HAS_ERRORED,
-        hasErrored: true
-    }
+  return {
+    type: FETCH_POSTS_HAS_ERRORED,
+    hasErrored: true
+  };
 }
 
 export function fetchPostsLoading(isLoading) {
-    return {
-        type: FETCH_POSTS_LOADING,
-        isLoading 
-    };
+  return {
+    type: FETCH_POSTS_LOADING,
+    isLoading
+  };
 }
 
 export function fetchPostsSuccessful(posts) {
-    return {
-        type: FETCH_POSTS_SUCCESSFUL,
-        items: posts 
-    };
+  return {
+    type: FETCH_POSTS_SUCCESSFUL,
+    items: posts
+  };
 }
 
 export function postsFetchData(url) {
-    return (dispatch) => {
-        dispatch(fetchPostsLoading(true));
+  return dispatch => {
+    dispatch(fetchPostsLoading(true));
 
-        fetch(url)
-            .then((response) => {
-                dispatch(fetchPostsLoading(false));
-                return response;
-            })
-            .then((response) => response.json())
-            .then((posts) => dispatch(fetchPostsSuccessful(posts)))
-            .catch((error) => {
-                dispatch(fetchPostsHasErrored()); 
-            });
-    };
+    fetch(url, { headers: { Authorization: "foobar" } })
+      .then(response => {
+        dispatch(fetchPostsLoading(false));
+        return response;
+      })
+      .then(response => response.json())
+      .then(posts => dispatch(fetchPostsSuccessful(posts)))
+      .catch(error => {
+        dispatch(fetchPostsHasErrored());
+      });
+  };
 }
 
 export function addComment({ author, body, postId }) {
-    return {
-        type: ADD_COMMENT,
-        author,
-        body,
-        postId
-    };
+  return {
+    type: ADD_COMMENT,
+    author,
+    body,
+    postId
+  };
 }
 
 export function likeComment({ commentId }) {
-    return {
-        type: LIKE_COMMENT,
-        commentId
-    };
+  return {
+    type: LIKE_COMMENT,
+    commentId
+  };
 }
 
 export function editComment({ author, body, commentId }) {
-    return {
-        type: EDIT_COMMENT,
-        author,
-        body,
-        commentId
-    };
+  return {
+    type: EDIT_COMMENT,
+    author,
+    body,
+    commentId
+  };
 }
 
 export function fetchCommentsHasErrored() {
-    return {
-        type: FETCH_COMMENTS_HAS_ERRORED,
-        hasErrored: true
-    }
+  return {
+    type: FETCH_COMMENTS_HAS_ERRORED,
+    hasErrored: true
+  };
 }
 
 export function fetchCommentsLoading(isLoading) {
-    return {
-        type: FETCH_COMMENTS_LOADING,
-        isLoading 
-    };
+  return {
+    type: FETCH_COMMENTS_LOADING,
+    isLoading
+  };
 }
 
 export function fetchCommentsSuccessful(posts) {
-    return {
-        type: FETCH_COMMENTS_SUCCESSFUL,
-        posts 
-    };
+  return {
+    type: FETCH_COMMENTS_SUCCESSFUL,
+    posts
+  };
 }
 
 export function commentsFetchData(url) {
-    return (dispatch) => {
-        dispatch(fetchCommentsLoading(true));
+  return dispatch => {
+    dispatch(fetchCommentsLoading(true));
 
-        fetch(url)
-            .then((response) => {
-                dispatch(fetchCommentsLoading(false));
-                return response;
-            })
-            .then((response) => response.json())
-            .then((comments) => dispatch(fetchCommentsSuccessful(comments)))
-            .catch(() => dispatch(fetchCommentsHasErrored()));
-    };
+    fetch(url, { headers: { Authorization: "foobar" } })
+      .then(response => {
+        dispatch(fetchCommentsLoading(false));
+        return response;
+      })
+      .then(response => response.json())
+      .then(comments => dispatch(fetchCommentsSuccessful(comments)))
+      .catch(() => dispatch(fetchCommentsHasErrored()));
+  };
 }
--- src/components/PostsContainer.js
+++ src/components/PostsContainer.js
@@ -1,81 +1,83 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import { postsFetchData } from "../actions/";
+import moment from "moment";
 
-class PostsContainer extends Component { 
-    componentDidMount() {
-      this.props.fetchData("http://59b9daa3a7ba690011e127d4.mockapi.io/api/v1/posts");      
-    }
-
-    render() {
-      if (this.props.hasErrored) {
-        return <p>Sorry!  There was an error</p>;
-      }
-      if (this.props.isLoading) {
-        return <p>Loading...</p>;
-      }
-      return (
-        <div>
-            <div className="ui equal width grid">
-              <div className="column">
-                Sort by&nbsp;&nbsp;
-                <div className="ui buttons">
-                      <button className="ui button" role="button">Date</button>
-                      <div className="or" />
-                      <button className="ui button" role="button">Votes</button>
-                </div>
-              </div>
-              <div className="column">
-                <div>Category: Sports | Politics | News</div>
-              </div>
-              <div className="column">
-                <div>
-                  <button className="ui icon right labeled primary button">Add New Post <i className="plus icon"></i></button>
-                </div>
-              </div>
-            </div>
-            <div className="ui cards">
-
-              {this.props.items.map((item) => (
-                <div className="ui fluid card">
-                  <div className="content">
-                    <div className="header">Lebron Scores 58 in OT Loss</div>
-                    <div className="meta">by John Doe, August 8th 2017, 9:53 AM</div>
-                  </div>
-                  <div className="extra content">
-                    <i className="thumbs up icon">485</i>
-                  </div>
-                </div>
-               ))}
-              <div className="ui fluid card">
-                <div className="content">
-                  <div className="header">Lebron Scores 58 in OT Loss</div>
-                  <div className="meta">by John Doe, August 8th 2017, 9:53 AM</div>
-                </div>
-                <div className="extra content">
-                  <i className="thumbs up icon">485</i>
-                </div>
-              </div>
-
-
-            </div>
-        </div>
-      );
-    }
+class PostsContainer extends Component {
+  componentDidMount() {
+    this.props.fetchData("http://localhost:3001/posts");
   }
 
-  const mapStateToProps = ({posts}) => {
-    return {
-      items: posts.items,
-      hasErrored: posts.hasErrored,
-      isLoading: posts.isLoading
-    };
-  };
+  render() {
+    if (this.props.hasErrored) {
+      return <p>Sorry! There was an error</p>;
+    }
+    if (this.props.isLoading) {
+      return <p>Loading...</p>;
+    }
+    return (
+      <div>
+        <div className="ui equal width grid">
+          <div className="column">
+            Sort by&nbsp;&nbsp;
+            <div className="ui buttons">
+              <button className="ui button" role="button">
+                Date
+              </button>
+              <div className="or" />
+              <button className="ui button" role="button">
+                Votes
+              </button>
+            </div>
+          </div>
+          <div className="column">
+            <div>Category: Sports | Politics | News</div>
+          </div>
+          <div className="column">
+            <div>
+              <button className="ui icon right labeled primary button">
+                Add New Post <i className="plus icon" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="ui cards">
+          {this.props.items.map(item =>
+            <div key={item.id} className="ui fluid card">
+              <div className="content">
+                <div className="header">
+                  {item.title}
+                </div>
+                <div className="meta">
+                  by {item.author},{" "}
+                  {moment.unix(item.timestamp).format("YYYY-MM-DD HH:mm")}
+                </div>
+              </div>
+              <div className="extra content">
+                <i className="thumbs up icon">
+                  {item.voteScore}
+                </i>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
 
-  const mapDispatchToProps = (dispatch) => {
-    return {
-      fetchData: (url) => dispatch(postsFetchData(url))
-    };
+const mapStateToProps = ({ posts }) => {
+  return {
+    items: posts.items,
+    hasErrored: posts.hasErrored,
+    isLoading: posts.isLoading
   };
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(PostsContainer);
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchData: url => dispatch(postsFetchData(url))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostsContainer);
