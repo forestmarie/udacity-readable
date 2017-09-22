daf1601350238e1892af2066a4c3ed90b6c9ea13
forestmarie <forest.marie@gmail.com>
2017-09-21 19:01:09 -0500

    feat: added ability to delete post

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,111 +1,107 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Button } from "semantic-ui-react";
-import { fetchPostDetails, voteOnPost } from "../actions/posts";
+import { fetchPostDetails, voteOnPost, deletePost } from "../actions/posts";
 import CommentsContainer from "./CommentsContainer";
 
 class PostDetail extends Component {
-    state = {
-        showComments: false,
-        voteChoice: "",
-        postId: this.props.match.params.id
-    };
+  state = {
+    showComments: false,
+    voteChoice: "",
+    postId: this.props.match.params.id
+  };
 
-    componentWillMount() {
-        this.props.fetchPost(`http://localhost:3001/posts/${this.state.postId}`);
+  componentWillMount() {
+    this.props.fetchPost(`http://localhost:3001/posts/${this.state.postId}`);
+  }
+
+  showComments = e => {
+    this.setState({
+      showComments: true
+    });
+  };
+
+  upVote = e => {
+    this.setState({
+      voteChoice: "up"
+    });
+
+    this.props.vote(`http://localhost:3001/posts/${this.state.postId}`, "upVote");
+  };
+
+  downVote = e => {
+    this.setState({
+      voteChoice: "down"
+    });
+    this.props.vote(`http://localhost:3001/posts/${this.state.postId}`, "downVote");
+  };
+
+  deletePost = () => {
+    this.props.deletePost(this.state.postId).then(_ => {
+      this.props.history.push("/posts");
+    });
+  };
+
+  render() {
+    const { currentPost, isLoading } = this.props;
+    const { voteChoice } = this.state;
+
+    if (currentPost) {
+      return (
+        <div className="ui fluid container">
+          <h2 className="ui header">{currentPost.title}</h2>
+          <div>
+            By {currentPost.author} {currentPost.timestamp}
+            <Button content="Edit" icon="file" labelPosition="right" />
+            <Button content="Delete" onClick={this.deletePost} icon="trash" labelPosition="right" />
+          </div>
+          <br />
+          <p>{currentPost.body}</p>
+          <br />
+          {!this.state.showComments && (
+            <div>
+              <Button disabled={voteChoice === "up"} onClick={this.upVote} icon="thumbs up" />
+              <Button disabled={voteChoice === "down"} onClick={this.downVote} icon="thumbs down" />
+              <button className="ui icon right labeled button" onClick={this.showComments}>
+                View Comments ({currentPost.voteScore}) <i className="comments icon" />
+              </button>
+            </div>
+          )}
+          <div className="ui divider" />
+          {this.state.showComments && <CommentsContainer />}
+        </div>
+      );
+    } else if (isLoading) {
+      return <div>Post details loading...</div>;
     }
-
-    showComments = e => {
-        this.setState({
-            showComments: true
-        });
-    };
-
-    upVote = e => {
-        this.setState({
-            voteChoice: "up"
-        });
-
-        this.props.vote(`http://localhost:3001/posts/${this.state.postId}`, "upVote");
-    };
-
-    downVote = e => {
-        this.setState({
-            voteChoice: "down"
-        });
-        this.props.vote(`http://localhost:3001/posts/${this.state.postId}`, "downVote");
-    };
-
-    render() {
-        const { currentPost, isLoading } = this.props;
-        const { voteChoice } = this.state;
-
-        if (currentPost) {
-            return (
-                <div className="ui fluid container">
-                    <h2 className="ui header">{currentPost.title}</h2>
-                    <div>
-                        By {currentPost.author} {currentPost.timestamp} <a href="edit">edit</a> |{" "}
-                        <a href="delete">delete</a>
-                    </div>
-                    <br />
-                    <p>{currentPost.body}</p>
-                    <br />
-                    {!this.state.showComments && (
-                        <div>
-                            <Button
-                                disabled={voteChoice === "up"}
-                                onClick={this.upVote}
-                                icon="thumbs up"
-                            />
-                            <Button
-                                disabled={voteChoice === "down"}
-                                onClick={this.downVote}
-                                icon="thumbs down"
-                            />
-                            <button
-                                className="ui icon right labeled button"
-                                onClick={this.showComments}
-                            >
-                                View Comments ({currentPost.voteScore}){" "}
-                                <i className="comments icon" />
-                            </button>
-                        </div>
-                    )}
-                    <div className="ui divider" />
-                    {this.state.showComments && <CommentsContainer />}
-                </div>
-            );
-        } else if (isLoading) {
-            return <div>Post details loading...</div>;
-        }
-        return null;
-    }
+    return null;
+  }
 }
 
 const mapStateToProps = ({ posts, common }) => {
-    let errorsFound = false;
-    if (common && common.errors && common.errors["fetch-post-details"]) {
-        errorsFound = true;
-    }
+  let errorsFound = false;
+  if (common && common.errors && common.errors["fetch-post-details"]) {
+    errorsFound = true;
+  }
 
-    let isLoading = false;
-    if (common && common.loading["fetch-post-details"]) {
-        isLoading = true;
-    }
+  let isLoading = false;
+  if (common && common.loading["fetch-post-details"]) {
+    isLoading = true;
+  }
 
-    return {
-        hasErrored: errorsFound,
-        isLoading: isLoading,
-        currentPost: posts.currentPost
-    };
+  return {
+    hasErrored: errorsFound,
+    isLoading: isLoading,
+    currentPost: posts.currentPost
+  };
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        fetchPost: url => dispatch(fetchPostDetails(url)),
-        vote: (url, voteChoice) => dispatch(voteOnPost(url, voteChoice))
-    };
+  return {
+    fetchPost: url => dispatch(fetchPostDetails(url)),
+    vote: (url, voteChoice) => dispatch(voteOnPost(url, voteChoice)),
+    deletePost: url => dispatch(deletePost(url))
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(PostDetail);
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,85 +1,85 @@
 import {
-    ADD_POST_SUCCESSFUL,
-    EDIT_POST,
-    FETCH_POSTS_SUCCESSFUL,
-    FETCH_POST_DETAILS_SUCCESSFUL,
-    FETCH_POSTS_BY_CATEGORY_SUCCESSFUL,
-    VOTE_ON_POST_SUCCESSFUL,
-    SORT_POSTS
+  ADD_POST_SUCCESSFUL,
+  EDIT_POST,
+  FETCH_POSTS_SUCCESSFUL,
+  FETCH_POST_DETAILS_SUCCESSFUL,
+  FETCH_POSTS_BY_CATEGORY_SUCCESSFUL,
+  DELETE_POST_SUCCESSFUL,
+  VOTE_ON_POST_SUCCESSFUL,
+  SORT_POSTS
 } from "../actions/posts";
 
 const postsInitialState = {
-    items: [],
-    hasErrored: false,
-    isLoading: false
+  items: []
 };
 
 const posts = (state = postsInitialState, action) => {
-    const { author, title, body, postId, voteScore } = action;
+  const { author, title, body, postId, voteScore } = action;
 
-    switch (action.type) {
-        case ADD_POST_SUCCESSFUL:
-            return {
-                ...state,
-                items: [...state.items, action.post]
-            };
+  switch (action.type) {
+    case ADD_POST_SUCCESSFUL:
+      return {
+        ...state,
+        items: [...state.items, action.post]
+      };
 
-        case EDIT_POST:
-            let posts = state.filter(x => x.postId !== postId);
-            return {
-                isLoading: state.isLoading,
-                hasErrored: state.hasErrored,
-                items: [...posts, { author, title, body, postId }]
-            };
+    case EDIT_POST:
+      let posts = state.filter(x => x.postId !== postId);
+      return {
+        isLoading: state.isLoading,
+        hasErrored: state.hasErrored,
+        items: [...posts, { author, title, body, postId }]
+      };
 
-        case VOTE_ON_POST_SUCCESSFUL:
-            const { currentPost } = state;
-            let newVoteScore = currentPost.voteScore + voteScore;
+    case VOTE_ON_POST_SUCCESSFUL:
+      const { currentPost } = state;
+      let newVoteScore = currentPost.voteScore + voteScore;
 
-            return {
-                ...state,
-                currentPost: { ...state.currentPost, voteScore: newVoteScore }
-            };
+      return {
+        ...state,
+        currentPost: { ...state.currentPost, voteScore: newVoteScore }
+      };
 
-        case FETCH_POSTS_SUCCESSFUL:
-            return {
-                hasErrored: false,
-                isLoading: state.isLoading,
-                items: action.items
-            };
+    case DELETE_POST_SUCCESSFUL:
+      return {
+        items: [...state.items.filter(x => x.id !== action.postId)]
+      };
 
-        case FETCH_POST_DETAILS_SUCCESSFUL:
-            return {
-                ...state,
-                currentPost: action.post
-            };
+    case FETCH_POSTS_SUCCESSFUL:
+      return {
+        items: action.items
+      };
 
-        case FETCH_POSTS_BY_CATEGORY_SUCCESSFUL:
-            return {
-                hasErrored: false,
-                isLoading: state.isLoading,
-                items: action.items
-            };
+    case FETCH_POST_DETAILS_SUCCESSFUL:
+      return {
+        ...state,
+        currentPost: action.post
+      };
 
-        case SORT_POSTS:
-            let currentPosts = [...state.items];
-            let sortKey = action.filter;
+    case FETCH_POSTS_BY_CATEGORY_SUCCESSFUL:
+      return {
+        items: action.items
+      };
 
-            return {
-                ...state,
-                items: currentPosts.sort((a, b) => {
-                    if (a[sortKey] < b[sortKey]) {
-                        return 1;
-                    } else if (a[sortKey] > b[sortKey]) {
-                        return -1;
-                    }
-                    return 0;
-                })
-            };
+    case SORT_POSTS:
+      let currentPosts = [...state.items];
+      let sortKey = action.filter;
 
-        default:
-            return state;
-    }
+      return {
+        ...state,
+        items: currentPosts.sort((a, b) => {
+          if (a[sortKey] < b[sortKey]) {
+            return 1;
+          } else if (a[sortKey] > b[sortKey]) {
+            return -1;
+          }
+          return 0;
+        })
+      };
+
+    default:
+      return state;
+  }
 };
 
 export default posts;
