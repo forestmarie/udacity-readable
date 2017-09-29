610872d2d8eeec3fea5701e7e8a1f5050d3f3db8
forestmarie <forest.marie@gmail.com>
2017-09-28 19:52:11 -0500

    refactor: moving to fetch service

--- src/features/comments/CommentActions.js
+++ src/features/comments/CommentActions.js
@@ -1,161 +1,123 @@
-import { baseFetchHeaders, BaseApiUrl } from "../../utils/http-helpers";
-import { fetchErrored, fetchLoading } from "../common/CommonActions";
-import toastr from "toastr";
-export const ADD = "ADD";
-export const DELETE = "DELETE";
-export const VOTE = "VOTE";
-export const EDIT = "EDIT";
-export const FETCH = "FETCH";
+import { fetchService } from "../../utils/http-helpers";
+export const ADD_COMMENT = "ADD_COMMENT";
+export const DELETE_COMMENT = "DELETE_COMMENT";
+export const VOTE_ON_COMMENT = "VOTE_COMMENT";
+export const EDIT_COMMENT = "EDIT_COMMENT";
+export const FETCH_COMMENTS = "FETCH_COMMENTS";
 
 export function addSuccessful(comment) {
-    return {
-        type: ADD,
-        comment: comment
-    };
+  return {
+    type: ADD_COMMENT,
+    comment: comment
+  };
 }
 
 export function addComment(postId, comment) {
-    return dispatch => {
-        dispatch(fetchLoading(ADD, true));
+  const request = {
+    ...comment,
+    parentId: postId
+  };
 
-        const request = {
-            ...comment,
-            parentId: postId
-        };
-
-        return fetch(`${BaseApiUrl}/comments`, {
-            headers: baseFetchHeaders,
-            method: "POST",
-            body: JSON.stringify(request)
-        })
-            .then(response => {
-                dispatch(fetchLoading(ADD, false));
-                return response;
-            })
-            .then(response => response.json())
-            .then(comment => {
-                dispatch(addSuccessful(comment));
-                toastr.info("Comment added successfully");
-            })
-            .catch(error => {
-                dispatch(fetchErrored(ADD));
-            });
-    };
+  return dispatch => {
+    return fetchService.post(
+      ADD_COMMENT,
+      "/comments",
+      "Comment",
+      JSON.stringify(request),
+      dispatch,
+      addSuccessful({ ...comment, voteScore: 1 })
+    );
+  };
 }
 
-export function editSuccessful(commentId, timestamp) {
-    return {
-        type: EDIT,
-        commentId,
-        timestamp
-    };
+export function editSuccessful(commentId, timestamp, body) {
+  return {
+    type: EDIT_COMMENT,
+    commentId,
+    timestamp,
+    body
+  };
 }
 
 export function editComment(commentId, body) {
-    return dispatch => {
-        dispatch(fetchLoading(EDIT, true));
+  const now = Date.now();
 
-        const now = Date.now();
+  const request = {
+    timestamp: now,
+    body: body
+  };
 
-        const request = {
-            timestamp: now,
-            body: body
-        };
-
-        return fetch(`${BaseApiUrl}/comments/${commentId}`, {
-            headers: baseFetchHeaders,
-            method: "PUT",
-            body: JSON.stringify(request)
-        })
-            .then(response => {
-                dispatch(fetchLoading(EDIT, false));
-                return response;
-            })
-            .then(response => response.json())
-            .then(comment => {
-                dispatch(editSuccessful(commentId, now));
-                toastr.info("Comment edited successfully");
-            })
-            .catch(error => {
-                dispatch(fetchErrored(ADD));
-            });
-    };
+  return dispatch => {
+    return fetchService.put(
+      EDIT_COMMENT,
+      `/comments/${commentId}`,
+      "Comment",
+      JSON.stringify(request),
+      dispatch,
+      editSuccessful(commentId, now, body)
+    );
+  };
 }
 
 export function voteSuccessful(commentId, choice) {
-    return {
-        type: VOTE,
-        commentId,
-        choice
-    };
+  return {
+    type: VOTE_ON_COMMENT,
+    commentId,
+    choice
+  };
 }
 
 export function vote(commentId, choice) {
-    const request = {
-        option: choice
-    };
+  const request = {
+    option: choice
+  };
 
-    return dispatch => {
-        dispatch(fetchLoading(VOTE, true));
-
-        return fetch(`${BaseApiUrl}/comments/${commentId}`, {
-            headers: baseFetchHeaders,
-            method: "POST",
-            body: JSON.stringify(request)
-        })
-            .then(response => {
-                dispatch(fetchLoading(VOTE, false));
-                return response;
-            })
-            .then(response => response.json())
-            .then(() => dispatch(voteSuccessful(commentId, choice)))
-            .catch(error => dispatch(fetchErrored(VOTE)));
-    };
+  return dispatch => {
+    return fetchService.post(
+      VOTE_ON_COMMENT,
+      `/comments/${commentId}`,
+      "Comment",
+      JSON.stringify(request),
+      dispatch,
+      voteSuccessful(commentId, choice)
+    );
+  };
 }
 
 export function deleteSuccessful(commentId) {
-    return {
-        type: DELETE,
-        commentId
-    };
+  return {
+    type: DELETE_COMMENT,
+    commentId
+  };
 }
 
 export function deleteComment(commentId) {
-    return dispatch => {
-        dispatch(fetchLoading(DELETE, true));
-
-        return fetch(`${BaseApiUrl}/comments/${commentId}`, {
-            headers: baseFetchHeaders,
-            method: "DELETE"
-        })
-            .then(response => {
-                dispatch(fetchLoading(DELETE, false));
-                return response;
-            })
-            .then(response => response.json())
-            .then(() => dispatch(deleteSuccessful(commentId)))
-            .catch(error => dispatch(fetchErrored(DELETE)));
-    };
+  return dispatch => {
+    return fetchService.delete(
+      FETCH_COMMENTS,
+      `/comments/${commentId}`,
+      "Comment",
+      deleteSuccessful(commentId),
+      dispatch
+    );
+  };
 }
 
 export function fetchSuccessful(comments) {
-    return {
-        type: FETCH,
-        comments
-    };
+  return {
+    type: FETCH_COMMENTS,
+    comments
+  };
 }
 
 export function fetchComments(postId) {
-    return dispatch => {
-        dispatch(fetchLoading(FETCH, true));
-
-        return fetch(`${BaseApiUrl}/posts/${postId}/comments`, { headers: baseFetchHeaders })
-            .then(response => {
-                dispatch(fetchLoading(FETCH, false));
-                return response;
-            })
-            .then(response => response.json())
-            .then(comments => dispatch(fetchSuccessful(comments)))
-            .catch(error => dispatch(fetchErrored(FETCH)));
-    };
+  return dispatch => {
+    return fetchService.get(
+      FETCH_COMMENTS,
+      `/posts/${postId}/comments`,
+      "Comment",
+      fetchSuccessful,
+      dispatch
+    );
+  };
 }
--- src/features/comments/CommentReducer.js
+++ src/features/comments/CommentReducer.js
@@ -1,56 +1,62 @@
-import { ADD, VOTE, DELETE, EDIT, FETCH } from "./CommentActions";
+import {
+  ADD_COMMENT,
+  VOTE_ON_COMMENT,
+  DELETE_COMMENT,
+  EDIT_COMMENT,
+  FETCH_COMMENTS
+} from "./CommentActions";
 
 const commentsInitialState = {
-    items: []
+  items: []
 };
 
 const comments = (state = commentsInitialState, action) => {
-    const { items } = state;
-    const { commentId, choice, timestamp } = action;
-    let index;
-    let originalComment;
+  const { items } = state;
+  const { commentId, choice, timestamp, body } = action;
+  let index;
+  let originalComment;
 
-    switch (action.type) {
-        case ADD:
-            return { items: [...items, { ...action.comment }] };
+  switch (action.type) {
+    case ADD_COMMENT:
+      return { items: [...items, { ...action.comment }] };
 
-        case DELETE:
-            index = items.findIndex(x => x.id === commentId);
+    case DELETE_COMMENT:
+      index = items.findIndex(x => x.id === commentId);
 
-            return {
-                items: [...items.slice(0, index), ...items.slice(index + 1, items.length)]
-            };
+      return {
+        items: [...items.slice(0, index), ...items.slice(index + 1, items.length)]
+      };
 
-        case VOTE:
-            originalComment = items.filter(x => x.id === commentId)[0];
-            index = items.findIndex(x => x.id === commentId);
+    case VOTE_ON_COMMENT:
+      originalComment = items.filter(x => x.id === commentId)[0];
+      index = items.findIndex(x => x.id === commentId);
 
-            const voteScore = choice === "upVote" ? 1 : -1;
+      const voteScore = choice === "upVote" ? 1 : -1;
 
-            const comment = {
-                ...originalComment,
-                voteScore: originalComment.voteScore + voteScore
-            };
+      const comment = {
+        ...originalComment,
+        voteScore: originalComment.voteScore + voteScore
+      };
 
-            return {
-                items: [...items.slice(0, index), comment, ...items.slice(index + 1, items.length)]
-            };
+      return {
+        items: [...items.slice(0, index), comment, ...items.slice(index + 1, items.length)]
+      };
 
-        case EDIT:
-            const unmodifiedComments = items.filter(x => x.id !== commentId);
-            originalComment = items.filter(x => x.id === commentId)[0];
-            const modifiedComment = { ...originalComment, timestamp: timestamp };
+    case EDIT_COMMENT:
+      const unmodifiedComments = items.filter(x => x.id !== commentId);
+      originalComment = items.filter(x => x.id === commentId)[0];
+      const modifiedComment = { ...originalComment, timestamp: timestamp, body: body };
 
-            return { items: [...unmodifiedComments, modifiedComment] };
+      return { items: [...unmodifiedComments, modifiedComment] };
 
-        case FETCH:
-            return {
-                items: [...action.comments]
-            };
+    case FETCH_COMMENTS:
+      return {
+        items: [...action.comments]
+      };
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 };
 
 export default comments;
